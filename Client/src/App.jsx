e648189a06ddc712950f719e0a658d0ccd7feaf7
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-02-17 12:45:57 +0530

    Navbar

---
+++ Client/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import { useLocation } from "react-router-dom";
+
+const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
+  const isOwnerPath = useLocation().pathname.startsWith("/owner");
+
+  return <>{!isOwnerPath && <Navbar setShowLogin={setShowLogin} />}</>;
+};
+
+export default App;
---
+++ Client/src/assets/assets.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import search_icon from "./search_icon.svg";
+import close_icon from "./close_icon.svg";
+import menu_icon from "./menu_icon.svg";
+
+export const assets = {
+  logo,
+  search_icon,
+  close_icon,
+  menu_icon,
+};
+
+export const menuLinks = [
+  { name: "Home", path: "/" },
+  { name: "Cars", path: "/cars" },
+  { name: "My Bookings", path: "/my-bookings" },
+];

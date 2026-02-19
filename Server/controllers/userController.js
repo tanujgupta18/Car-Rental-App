38951398b8e28cf7dfbbf9e89d764aa77968bc46
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-02-19 19:57:18 +0530

    Users API

---
+++ Server/controllers/userController.js
@@ -0,0 +1,51 @@
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const generateToken = (userId) => {
+  const payload = userId;
+  return jwt.sign(payload, process.env.JWT_SECRET);
+};
+
+// API to register user
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password || password.length < 8) {
+      return res.json({ success: false, message: "Fill all the fields" });
+    }
+
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ name, email, password: hashedPassword });
+    const token = generateToken(user._id.toString());
+    res.json({ success: true, token });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// API to login user
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.json({ success: false, message: "Invalid Credentials" });
+    }
+    const token = generateToken(user._id.toString());
+    res.json({ success: true, token });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
--- Server/server.js
+++ Server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 connectDB();
@@ -12,6 +13,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("API is Working");
 });
+app.use("/api/user", userRouter);
 
 const PORT = process.envPORT || 3000;
 app.listen(PORT, () => {

468cecab758cd48ed1dfc814ca8d33932bbfc350
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-02-21 09:51:42 +0530

    Fix

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -12,6 +12,6 @@ const userRouter = express.Router();
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/data", protect, getUserData);
-userRouter.get("/cars", protect, getCars);
+userRouter.get("/cars", getCars);
 
 export default userRouter;

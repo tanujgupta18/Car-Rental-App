d02f1aab1935519761453608bacd3ad2d2e05ef1
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-02-22 00:09:07 +0530

    ManageBookings - Page

--- Client/src/pages/owner/ManageBookings.jsx
+++ Client/src/pages/owner/ManageBookings.jsx
@@ -1,13 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Title from "../../components/owner/Title";
-import { assets, dummyMyBookingsData } from "../../assets/assets";
+import { AppContext } from "../../context/AppContext";
+import toast from "react-hot-toast";
 
 const ManageBookings = () => {
-  const currency = import.meta.env.VITE_CURRENCY;
+  const { currency, axios } = useContext(AppContext);
+
   const [bookings, setBookings] = useState([]);
 
   const fetchOwnersBookings = async () => {
-    setBookings(dummyMyBookingsData);
+    try {
+      const { data } = await axios.get("/api/bookings/owner");
+      data.success ? setBookings(data.bookings) : toast.error(data.message);
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
+  const changeBookingStatus = async (bookingId, status) => {
+    try {
+      const { data } = await axios.post("/api/bookings/change-status", {
+        bookingId,
+        status,
+      });
+
+      if (data.success) {
+        toast.success(data.message);
+        fetchOwnersBookings();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   useEffect(() => {
@@ -70,12 +95,15 @@ const ManageBookings = () => {
                 <td className="p-3">
                   {booking.status === "pending" ? (
                     <select
+                      onChange={(e) =>
+                        changeBookingStatus(booking._id, e.target.value)
+                      }
                       value={booking.status}
                       className="px-2 py-1.5 mt-1 text-gray-500 border border-borderColor rounded-md outline-none"
                     >
                       <option value="pending">Pending</option>
                       <option value="confirmed">Confirmed</option>
-                      <option value="completed">Completed</option>
+                      <option value="cancelled">Cancelled</option>
                     </select>
                   ) : (
                     <span

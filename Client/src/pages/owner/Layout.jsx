f12b24e625c2372117f50fca9ee0b1a9192b643b
Tanuj Gupta <tanujgupta1872003@gmail.com>
2026-02-21 18:24:49 +0530

    Owner Layout

--- Client/src/pages/owner/Layout.jsx
+++ Client/src/pages/owner/Layout.jsx
@@ -1,8 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
 import NavbarOwner from "../../components/owner/NavbarOwner";
 import Sidebar from "../../components/owner/Sidebar";
+import { useContext } from "react";
+import { AppContext } from "../../context/AppContext";
 
 const Layout = () => {
+  const { isOwner } = useContext(AppContext);
+
+  if (isOwner === false) return <Navigate to="/" replace />;
+
   return (
     <div className="flex flex-col">
       <NavbarOwner />
